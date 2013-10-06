f1dce7be818cca6958f04d52fcb1575ce829f048
Patrick Murray <patfmurray@gmail.com>
2013-10-05 23:07:36 -0400

    changed the format to json, will be useful later

--- dirCMD.py
+++ dirCMD.py
@@ -1,59 +1,44 @@
 import os
 import sys
+import json
 import argparse
 from xml.sax.saxutils import quoteattr as xml_quoteattr
+from collections import defaultdict
+from pprint import pprint
 
-# Settings
+def set_leaf(root, dirs, item):
 
-
-# Does not return items that have a period as the first character (used to ignore OS X hidden files)
-def nodot(item): return item[0] != '.'
-
-
-
-def xmlCrawler(path):
-    result = ''
-    for item in filter(nodot, os.listdir(path)):
-        itempath = os.path.join(path, item)
-        # If the program comes across another subdirectory it restarts the script and continues down the tree
-        if os.path.isdir(itempath):
-            result += '<dir name="'+xmlCrawler(itempath)+'">'
-        # If the program comes across a file it uses the file's path to pull out the file name and give it a "file" classification
-        elif os.path.isfile(itempath):
-            result += '<file name="'+item+'">'
-    
-    #Adds closing markup after the loop
-    output = result
-    return output
-
+    if len(dirs) == 1:
+        root[dirs[0]] = item
+        return
+    if not root.has_key(dirs[0]):
+        root[dirs[0]] = {}
+    set_leaf(root[dirs[0]], dirs[1:], item)
 
 def main(argv):                         
-    parser = argparse.ArgumentParser(description='Print a directory tree.')
+    parser = argparse.ArgumentParser(description='Print a directory root.')
     parser.add_argument('path', metavar='P',
-                       help='starting path for the tree traversal')
+                       help='starting path for the root traversal')
     parser.add_argument('-v', '--verbose', help='print directory traversal process (increases running time)', action="store_true")
 
     args = parser.parse_args()
     verbose = args.verbose
     path = args.path 
   
-
-    #try:                                
-    #    opts, args = getopt.getopt(argv, "hp:v", ["help", "path="]) 
-    #except getopt.GetoptError:           
-    #    usage()                          
-    #    sys.exit(2)   
-
-    print path  
-    print xmlCrawler(path)
+    startpath = path
+    root = {}
+    for root, dirs, files in os.walk(startpath):
+        dirs = [startpath]
+        if root != startpath:
+            dirs.extend(os.path.relpath(root, startpath).split('/'))
     
-    # Flags: output file location and name, path, verbose, help, json/xml/other?
+        set_leaf(root, dirs, dict([(d,{}) for d in dirs]+ \
+                                      [(f,None) for f in files]))
         
+    pprint(root)
+    
+
         
 if __name__ == '__main__':
     
     main(sys.argv[1:])
-    
-    
-    
-    
